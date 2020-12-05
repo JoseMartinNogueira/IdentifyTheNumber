e0f80435d2826b04c44f06ef9b014d51ab133bcd
JoseMartinNogueira <jmartin.nogueira@gmail.com>
2020-12-05 23:46:36 +0100

    Delivery

---
+++ Assets/Scripts/ShowTextScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTextScript : MonoBehaviour
+{
+    public string numberToShow;
+    private Animator textAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textAnimator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void RestartAnimation()
+    {
+       textAnimator.SetTrigger("Restart");
+    }
+}
